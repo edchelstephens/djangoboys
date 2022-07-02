20c618ab21794750914be8227db67b82f129f239
Edchel Stephen Nini <edchelstephens@gmail.com>
2022-07-02 09:34:07 +0800

    feat: index shows published posts.

--- blogs/views/post.py
+++ blogs/views/post.py
@@ -1,7 +1,22 @@
 from django.views.generic import TemplateView
+from django.utils import timezone
+from django.db.models import QuerySet
+
+from blogs.models.post import Post
 
 class PostListTemplateView(TemplateView):
     """Post lists template view."""
     template_name = "blogs/post_lists.html"
+
+    def get_posts(self) -> QuerySet:
+        """Get published posts up to this point."""
+        posts = Post.objects.filter(published_at__lte=timezone.now()).order_by("published_at")
+        return posts
+
+    def get_context_data(self, **kwargs) -> dict:
+        """Get context to be rendered on template."""
+        context = super().get_context_data(**kwargs)
+        context["posts"] = self.get_posts()
+        return context
 
     

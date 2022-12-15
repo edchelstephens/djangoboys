bf6efff7ee3c197248e5bc56aa3682000103ffcb
Edchel Stephen Nini <edchelstephens@gmail.com>
2022-12-16 07:59:26 +0800

    feat: auto reload on development server with django-browser-reload.

--- djangoboys/settings/dev.py
+++ djangoboys/settings/dev.py
@@ -1,9 +1,9 @@
-from djangoboys.settings.base import INSTALLED_APPS, MIDDLEWARE
+from djangoboys.settings.base import *  # noqa
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
 DEVELOPMENT_APPS = ["django_extensions", "django_browser_reload"]
 DEVELOPMENT_MIDDLEWARES = ["django_browser_reload.middleware.BrowserReloadMiddleware"]
 
-INSTALLED_APPS += DEVELOPMENT_APPS
-MIDDLEWARE += DEVELOPMENT_MIDDLEWARES
+INSTALLED_APPS += DEVELOPMENT_APPS  # noqa
+MIDDLEWARE += DEVELOPMENT_MIDDLEWARES  # noqa

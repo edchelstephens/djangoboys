ea970c420fb8549a3b10af1f31c68911adfb9482
Edchel Stephen Nini <edchelstephens@gmail.com>
2023-01-06 07:39:32 +0800

    settings: production.

--- djangoboys/settings/production.py
+++ djangoboys/settings/production.py
@@ -1,3 +1,8 @@
 from djangoboys.settings.base import *  # noqa
 
 DEBUG = False
+
+ALLOWED_HOSTS = [
+    "djangodeploy-sandbox-env1.eba-q3ysh3us.ap-southeast-2.elasticbeanstalk.com",
+    "172.31.39.247",
+]

680407ec708c63dd07d6da27d7ff2d912adc316e
Edchel Stephen Nini <edchelstephens@gmail.com>
2023-01-06 12:44:08 +0800

    ci: update allowed hosts.

--- djangoboys/settings/production.py
+++ djangoboys/settings/production.py
@@ -3,7 +3,5 @@ from djangoboys.settings.base import *  # noqa
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "djangodeploy-sandbox-env1.eba-q3ysh3us.ap-southeast-2.elasticbeanstalk.com",
-    "172.31.39.247",
-    "*",
+    "djangodeploy-sandbox-env1.eba-abiastx8.ap-southeast-2.elasticbeanstalk.com",
 ]

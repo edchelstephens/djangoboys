990d1cf99af7e2845d0b41997707ec60b0e9f903
Edchel Stephen Nini <edchelstephens@gmail.com>
2023-01-06 13:05:05 +0800

    ci: settings.

--- .platform/hooks/predeploy/01_migrations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /var/app/venv/*/bin/activate
-
-
-python3 manage.py migrate
--- .platform/hooks/predeploy/02_collectstatic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /var/app/venv/*/bin/activate
-cd /var/app/staging
-
-python manage.py collectstatic
